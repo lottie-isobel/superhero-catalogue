3ec4c8ab2b29e0828f257c5d792cd30f6969577b
90 <hi@90.gripe>
2024-03-01 12:45:23 +0000

    Fully implement search/filter functionality

--- src/pages/AllHeroes.jsx
+++ src/pages/AllHeroes.jsx
@@ -3,6 +3,7 @@ import {HeroCard} from "../components/index.js";
 
 export default function AllHeroes() {
     const [characters, setCharacters] = useState([]);
+    const [searchText, setSearchText] = useState("");
 
     useEffect(() => {
         async function displayCharacters() {
@@ -18,11 +19,16 @@ export default function AllHeroes() {
         <div>
             <h1>All Superheroes</h1>
             <form className="search-form">
-                <input type="text" placeholder="enter a hero"></input>
+                <input type="text"
+                       value={searchText}
+                       onChange={e => setSearchText(e.target.value.toLowerCase())}
+                       placeholder="enter a hero"></input>
                 <button type="submit">Search</button>
             </form>
             <div className="grid-container">
-                {characters.map((hero, index) => <HeroCard hero={hero} key={index} />)}
+                {characters
+                    .filter(c => !searchText || c.name.toLowerCase().includes(searchText))
+                    .map((hero, index) => <HeroCard hero={hero} key={index}/>)}
             </div>
         </div>
     );
